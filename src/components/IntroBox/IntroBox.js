1c23721d7d84ebee256c674f52d81d3e216140c0
DNS <djunsonik@gmail.com>
2023-10-21 14:56:09 +0300

    added header and sketch of main page

---
+++ src/components/IntroBox/IntroBox.js
@@ -0,0 +1,12 @@
+import "./IntroBox.scss";
+
+const IntroBox = () => {
+    return(
+        <div className="introduction-box">
+            <div className="chessboard-box"></div>
+            <div className="text-box"></div>
+        </div>
+    )
+}
+
+export default IntroBox;
