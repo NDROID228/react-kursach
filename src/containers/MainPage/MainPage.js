6ad0cbed7131a46b52e74f29f4eadcb6e2f96878
DNS <djunsonik@gmail.com>
2023-10-24 19:10:21 +0300

    made random vs random chess intro

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -1,16 +1,32 @@
 import Header from "../../components/Header/Header";
 import IntroBox from "../../components/IntroBox/IntroBox";
+import { useState } from "react";
 import "./MainPage.scss";
 
 function MainPage() {
+  // const [boardConfigArr, setBoardConfigArr] = useState([]);
+
+  // useState(() => {
+  //   setBoardConfigArr([
+  //     {
+  //       id: "intro_board"
+  //     }
+  //   ])
+  // }, []);
+  const boardConfigArr = [
+    {
+      id: "intro_board",
+    },
+  ];
+
   return (
     <div className="container">
       <Header />
       <main>
         <div className="main-content">
-          <IntroBox />
-          <IntroBox />
-          <IntroBox />
+          {boardConfigArr.map((config) => {
+            return <IntroBox boardConfig={config} key={`box-${config.id}`}/>;
+          })}
         </div>
       </main>
     </div>
